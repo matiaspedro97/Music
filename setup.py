dfcd5e1c6b216fa62886b44eb8e011b6554752f3
matiaspedro97 <matiaspedro97@gmail.com>
2023-09-03 14:29:12 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo with some approaches aiming to get a Music Genre Classifier',
+    author='Pedro Matias',
+    license='MIT',
+)
